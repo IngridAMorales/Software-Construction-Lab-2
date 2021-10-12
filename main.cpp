1a7f742cda04c05fa41eff9ccc0f6ab3328d5bc2
IngridAMorales <ingridmorales1016@gmail.com>
2021-10-11 17:47:55 -0700

    Adding function to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
+#include "c-echo.h"
 
 int main()
 {
 <<<<<<< HEAD
+
     std::string name;
     std::cout << "What is your name?" << std::endl;
     std::cin >> name;
@@ -12,4 +14,7 @@ int main()
     return 0;
 
 >>>>>>> 8bc74f8086892f13d803ec6d3cfed7e0c47b1c08
+}
+int main(int argv, char** argc) {
+	std::cout<< "Count of output: " << count(echo(argv,argc)) <<std::endl;
 }
